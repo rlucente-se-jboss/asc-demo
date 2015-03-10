265a5c7e9c29c87bc2abdfa5bae2dd12942d3f29
Rich Lucente <rlucente@Mistis-MacBook-Air.local>
2015-03-09 22:19:43 -0400

    add second database

--- mysql-osx-config.sh
+++ mysql-osx-config.sh
@@ -3,11 +3,19 @@
 /usr/local/mysql-5.5.42-osx10.8-x86_64/bin/mysql -u root <<END1
 
 GRANT USAGE ON *.* TO 'bpmdemo'@'localhost' IDENTIFIED BY 'demo!123';
+GRANT USAGE ON *.* TO 'notedb'@'localhost' IDENTIFIED BY 'demo!123';
+
 DROP USER 'bpmdemo'@'localhost';
+DROP USER 'notedb'@'localhost';
+
 DROP DATABASE IF EXISTS bpmdemo;
+DROP DATABASE IF EXISTS notedb;
 
 CREATE DATABASE bpmdemo;
+CREATE DATABASE notedb;
+
 GRANT ALL PRIVILEGES ON bpmdemo.* TO 'bpmdemo'@'localhost' IDENTIFIED BY 'demo!123';
+GRANT ALL PRIVILEGES ON notedb.* TO 'notedb'@'localhost' IDENTIFIED BY 'demo!123';
 
 quit
 
